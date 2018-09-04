b16fbbe626db8075b8b39878776d8441811d932d
John Slater <johngslater@gmail.com>
2018-09-04 14:06:51 -0700

    pasted getServerState.js from master as newbie work around

--- server/getServerState.js
+++ server/getServerState.js
@@ -1,5 +1,5 @@
 console.log('loading getServerState.js');
-
+ 
 var http       = require('http');
 var fs         = require('fs');
 const socketIo = require("socket.io");
@@ -18,12 +18,50 @@ model.configure(5,4);
 range(31).map(()=> model.doRandomOp()); 
 
 // Loading the file index.html displayed to the client
-var server = http.createServer(function(req, res) {
-    fs.readFile('./server/getServerState.html', 'utf-8', function(error, content) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(content);
-    });
+//var server = http.createServer(function(req, res) {
+//    fs.readFile('./server/getServerState.html', 'utf-8', function(error, content) {
+//        res.writeHead(200, {"Content-Type": "text/html"});
+//        res.end(content);
+//    });
+//});
+
+
+var server =  http.createServer(function(request, response) {
+
+	if(request.url === "/index"){
+		sendFileContent(response, "server/getServerState.html", "text/html");
+	}
+	else if(request.url === "/"){
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write('<b>Hey there!</b><br /><br />This is the default response. Requested URL is: ' + request.url);
+	}
+	else if(/^\/[a-zA-Z0-9\/]*.js$/.test(request.url.toString())){
+		sendFileContent(response, request.url.toString().substring(1), "text/javascript");
+	}
+	else if(/^\/[a-zA-Z0-9\/]*.css$/.test(request.url.toString())){
+		sendFileContent(response, request.url.toString().substring(1), "text/css");
+	}
+	else{
+		console.log("Requested URL is: " + request.url);
+		response.end();
+	}
 });
+
+function sendFileContent(response, fileName, contentType){
+    fs.readFile(fileName, function(err, data){
+	if(err){
+	   response.writeHead(404);
+	   response.write("Not Found!");
+	}
+	else{
+	   response.writeHead(200, {'Content-Type': contentType});
+	   response.write(data);
+	}
+	response.end();
+    });
+}
+
+
 const portId = 8060
 server.listen( portId,()=> console.log('listening on *:' + portId) );
 
